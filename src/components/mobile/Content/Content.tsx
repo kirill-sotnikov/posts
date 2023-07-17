3f84bae0d3b4154160bf649ae516cf7f8572d84b
Kirill Sotnikov <sot-ks@ya.ru>
2023-07-17 22:02:16 +0300

    chore: add popup

--- src/components/mobile/Content/Content.tsx
+++ src/components/mobile/Content/Content.tsx
@@ -1,23 +1,22 @@
 import axios from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { useSearch } from "../../../searchContext";
 import { PostType } from "../../desktop/Content/Content";
 import { Loader } from "../../desktop/Loader/Loader";
+import { PopUp } from "../PopUp/PopUp";
 import { Post } from "../Post/Post";
 import style from "./Content.module.css";
 
 export const Content = () => {
   const { search } = useSearch();
+  const [selectedPost, setSelectedPost] = useState<PostType>(null);
   const { isLoading, error, data } = useQuery("repoData", () =>
     axios
       .get<Array<PostType>>(
         "https://cloud.codesupply.co/endpoint/react/data.json"
       )
-      .then((res) => {
-        console.log(res.data);
-
-        return res.data;
-      })
+      .then((res) => res.data)
   );
 
   if (error) {
@@ -33,9 +32,17 @@ export const Content = () => {
   if (data) {
     return (
       <div className={style.content}>
+        {selectedPost && (
+          <PopUp
+            {...selectedPost}
+            onClickOutside={() => setSelectedPost(null)}
+          />
+        )}
+
         {search.length === 0
           ? data.map((item, index) => (
               <Post
+                onClick={() => setSelectedPost(item)}
                 style={{
                   margin: index === 0 ? "0 auto" : "32px auto 0 auto",
                 }}
---
+++ src/components/mobile/PopUp/PopUp.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { FunctionComponent, useEffect, useState } from "react";
+import crossMobileMenu from "../../../../public/crossMobileMenu.svg";
+import dot from "../../../../public/dot.svg";
+import { PostType } from "../../desktop/Content/Content";
+import style from "./PopUp.module.css";
+
+interface PopUpProps extends PostType {
+  onClickOutside: () => void;
+}
+
+export const PopUp: FunctionComponent<PopUpProps> = ({
+  img_2x,
+  tags,
+  title,
+  autor,
+  date,
+  views,
+  text,
+  onClickOutside,
+}) => {
+  const [posY, setPosY] = useState(0);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    setPosY(window.scrollY);
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <div
+      className={style.wrapper}
+      onClick={(event) => {
+        if (event.target === event.currentTarget) {
+          console.log("me");
+          onClickOutside();
+        }
+      }}
+      style={{ top: posY }}
+    >
+      <div className={style.popUp}>
+        <div style={{ height: 230, position: "relative" }}>
+          <Image src={img_2x} alt="img" objectFit="cover" layout="fill" />
+        </div>
+        <p className={style.tag}>{tags}</p>
+        <p className={style.title}>{title}</p>
+        <p className={style.info}>
+          {autor} <Image src={dot} alt="dot" style={{ margin: "0 5px" }} />
+          <span>{date}</span>
+          <Image src={dot} alt="dot" style={{ margin: "0 5px" }} />
+          <span>{views}</span>
+        </p>
+        <p className={style.description}>{text}</p>
+        <div className={style.cross} onClick={onClickOutside}>
+          <Image src={crossMobileMenu} alt="cross" />
+        </div>
+      </div>
+    </div>
+  );
+};
